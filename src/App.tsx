59d11c5a7d7f2f8c73f063ed8be848ba9a67a217
Özkan Öztürk <ozkozturk@yandex.com>
2020-09-11 20:07:41 +0300

    added StyledAppLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { FC } from 'react';
+import styled from '@emotion/styled';
 import TaskInput from './components/TaskInput';
 import TasksList from './components/TasksList';
 
-function App() {
+const StyledAppLayout = styled.div`
+  width: 40%;
+  min-height: 400px;
+  background-color: #FF6666;
+  box-shadow: 1px 4px 12px 3px rgba(87,7,19,0.36);
+  border-radius: 5px;
+  margin: 100px auto;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  
+  h1 {
+    border-bottom: 1px solid #FFF;
+    font-size: 2em;
+  }
+`;
+
+const App: FC = () => {
   return (
-    <div>
+    <StyledAppLayout>
       <TaskInput />
       <TasksList />
-    </div>
+    </StyledAppLayout>
   );
-}
+};
 
 export default App;
