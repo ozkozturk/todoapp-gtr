f28bc5e952164634ae34d7fa7c22906a32ba2622
Özkan Öztürk <ozkozturk@yandex.com>
2020-09-12 02:52:44 +0300

    added crud actions

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -1,5 +1,28 @@
 import api from './api';
-import { GET_TASKS_PENDING, GET_TASKS_SUCCESS, GET_TASKS_FAILURE, GET_TASKS_FULFILL } from './actionTypes';
+import {
+  GET_TASKS_PENDING,
+  GET_TASKS_SUCCESS,
+  GET_TASKS_FAILURE,
+  GET_TASKS_FULFILL,
+  ADD_TASKS_PENDING,
+  ADD_TASKS_SUCCESS,
+  ADD_TASKS_FAILURE,
+  ADD_TASKS_FULFILL,
+  UPDATE_TASKS_PENDING,
+  UPDATE_TASKS_SUCCESS,
+  UPDATE_TASKS_FAILURE,
+  UPDATE_TASKS_FULFILL,
+  DELETE_TASKS_PENDING,
+  DELETE_TASKS_SUCCESS,
+  DELETE_TASKS_FAILURE,
+  DELETE_TASKS_FULFILL,
+} from './actionTypes';
+
+type UpdateTypes = {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 export const getTasks = () => {
   return (dispatch: any) => {
@@ -24,4 +47,70 @@ export const getTasks = () => {
   };
 };
 
-export const updateTask = () => {};
+export const addTask = (title:string) => {
+  return (dispatch: any) => {
+    dispatch({
+      type: ADD_TASKS_PENDING,
+    });
+    return api.post('/create', { title }).then((response) => {
+      dispatch({
+        type: ADD_TASKS_SUCCESS,
+        payload: response.data,
+      });
+    }).catch((error) => {
+      dispatch({
+        type: ADD_TASKS_FAILURE,
+        error,
+      });
+    }).finally(() => {
+      dispatch({
+        type: ADD_TASKS_FULFILL,
+      });
+    });
+  };
+};
+
+export const updateTask = ({ id, title, completed }: UpdateTypes) => {
+  return (dispatch: any) => {
+    dispatch({
+      type: UPDATE_TASKS_PENDING,
+    });
+    return api.post('/create', { id, title, completed }).then((response) => {
+      dispatch({
+        type: UPDATE_TASKS_SUCCESS,
+        payload: response.data,
+      });
+    }).catch((error) => {
+      dispatch({
+        type: UPDATE_TASKS_FAILURE,
+        error,
+      });
+    }).finally(() => {
+      dispatch({
+        type: UPDATE_TASKS_FULFILL,
+      });
+    });
+  };
+};
+export const deleteTask = (id: number, title: string) => {
+  return (dispatch: any) => {
+    dispatch({
+      type: DELETE_TASKS_PENDING,
+    });
+    return api.post('/create', { id, title }).then((response) => {
+      dispatch({
+        type: DELETE_TASKS_SUCCESS,
+        payload: response.data,
+      });
+    }).catch((error) => {
+      dispatch({
+        type: DELETE_TASKS_FAILURE,
+        error,
+      });
+    }).finally(() => {
+      dispatch({
+        type: DELETE_TASKS_FULFILL,
+      });
+    });
+  };
+};
