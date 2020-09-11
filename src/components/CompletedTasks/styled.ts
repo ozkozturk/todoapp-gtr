d345e7928dc94269573b7d2ff278c04c47266e63
Özkan Öztürk <ozkozturk@yandex.com>
2020-09-11 22:34:35 +0300

    styled CompletedTasks component

--- src/components/CompletedTasks/styled.ts
+++ src/components/CompletedTasks/styled.ts
@@ -1,7 +1,15 @@
 import styled from '@emotion/styled';
 
 const StyledCompletedTasks = styled.div`
-
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  
+  h1 {
+    margin: 50px auto;
+    width: 100%;
+  }
 `;
 
 export default StyledCompletedTasks;
