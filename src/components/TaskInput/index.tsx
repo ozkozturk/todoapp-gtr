4184804d1c2464375dc40dadd04d955f6c0072d5
Özkan Öztürk <ozkozturk@yandex.com>
2020-09-11 23:05:58 +0300

    refactored TaskInput

--- src/components/TaskInput/index.tsx
+++ src/components/TaskInput/index.tsx
@@ -1,12 +1,17 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { TaskInputProps } from './types';
 import StyledTaskInput from './styled';
 
 const TaskInput: FC<TaskInputProps> = () => {
+  const [task, setTask] = useState('');
+  const handleSubmit = (e:any) => {
+    e.preventDefault();
+    setTask('');
+  };
   return (
     <StyledTaskInput>
-      <form action="">
-        <input type="text" value="enter task" />
+      <form action="" onSubmit={handleSubmit}>
+        <input type="text" value={task} onChange={(e) => setTask(e.target.value)} />
         <button type="submit">add</button>
       </form>
     </StyledTaskInput>
