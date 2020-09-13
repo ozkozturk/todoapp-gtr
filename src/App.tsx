56f66839304403ed41d4a4e4cc79d443166159b8
Özkan Öztürk <ozkozturk@yandex.com>
2020-09-13 18:08:04 +0300

    styled headers

--- src/App.tsx
+++ src/App.tsx
@@ -4,31 +4,39 @@ import TaskInput from './components/TaskInput';
 import TasksList from './components/TasksList';
 
 const StyledAppLayout = styled.div`
-  width: 40%;
+  max-width: 450px;
+  width: 85%;
   min-height: 400px;
   background-color: #FF6666;
   box-shadow: 1px 4px 12px 3px rgba(87,7,19,0.36);
   border-radius: 5px;
-  margin: 100px auto;
+  font-family: 'Roboto', sans-serif;
+  margin: 80px auto;
   padding: 20px;
   display: flex;
   align-items: center;
   flex-direction: column;
   
-  h1 {
+  h1, h2 {
     border-bottom: 1px solid #FFF;
+  }
+  
+  h1 {
     font-size: 2em;
+  }
+  
+  h2 {
+    font-size: 1.8em;
+    margin-bottom: 10px;
   }
 `;
 
-const App: FC = () => {
-  return (
-    <StyledAppLayout>
-      <h1>THINGS TO DO</h1>
-      <TaskInput />
-      <TasksList />
-    </StyledAppLayout>
-  );
-};
+const App: FC = () => (
+  <StyledAppLayout>
+    <h1>THINGS TO DO</h1>
+    <TaskInput />
+    <TasksList />
+  </StyledAppLayout>
+);
 
 export default App;
